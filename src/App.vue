4bb471ea1f8da3239d48f45bd6ac49c2f2583818
Noah Mutersbaugh <nmutersb@gmail.com>
2021-03-17 13:23:00 -0400

    git folder rearranged

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>VueJS Experiment by {{ me }}</h1>
+    <h2>Personal Info</h2>
+    Name: {{ myInfo.name }}<br />
+    Semester: {{ myInfo.semester }} {{ myInfo.year }}<br />
+
+    Courses I am taking
+    <table>
+      <thead>
+        <tr>
+          <th>Course</th>
+          <th>Credits</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(c, pos) in myInfo.courses" :key="pos">
+          <td>{{ c.name }}</td>
+          <td class="credit-number">{{ c.credits }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <CourseCard
+      v-for="(course, pos) in myInfo.courses"
+      :key="pos"
+      :name="course.name"
+      :credits="course.credits"
+      :meetings="course.meetings"
+    ></CourseCard>
+
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <hello-world msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import CourseCard from "./components/CourseCard.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
     HelloWorld,
+    CourseCard,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  readonly me = "Noah Mutersbaugh";
+  readonly myInfo = {
+    name: "Noah Mutersbaugh",
+    semester: "Winter",
+    year: 2021,
+    courses: [
+      /* List the classes you are taking this semester */
+      {
+        name: "CIS 290: Professional Responsibilities and Practices",
+        credits: 3,
+        meetings: [{ day: "Tuesday", time: "10-11a" }],
+      },
+      {
+        name: "CIS 351: Computer Organization and Assembly Language",
+        credits: 4,
+        meetings: [
+          { day: "Monday", time: "4-5:15p" },
+          { day: "Wednesday", time: "6-8p" },
+        ],
+      },
+      {
+        name: "CIS 367: Computer Graphics",
+        credits: 3,
+        meetings: [
+          { day: "Tuesday", time: "4-5:15p" },
+          { day: "Thursday", time: "4-5:15p" },
+        ],
+      },
+      {
+        name: "CIS 371: Web Application Programming",
+        credits: 3,
+        meetings: [
+          { day: "Monday", time: "1-2p" },
+          { day: "Wednesday", time: "1-2p" },
+          { day: "Friday", time: "1-2p" },
+        ],
+      },
+      {
+        name: "CIS 457: Data Communications",
+        credits: 4,
+        meetings: [{ day: "Friday", time: "8-9a" }],
+      },
+    ],
+  };
+}
 </script>
 
 <style>
@@ -22,8 +98,26 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-flow: column;
+  width: auto;
+}
+
+table,
+td,
+th,
+tr {
+  border: 1px solid black;
+}
+
+.credit-number {
+  text-align: center;
+}
+
+img {
+  max-width: 200px;
 }
 </style>
---
+++ src/components/CourseCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="crs">
+    <h3>Course: {{ name }}</h3>
+    <h3>Credits: {{ credits }}</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Day</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr v-for="(meeting, pos) in meetings" :key="pos"><td>{{ meeting.day }}</td><td>{{ meeting.time }}</td></tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class CourseCard extends Vue {
+  @Prop() readonly name!: string;
+  @Prop() readonly credits!: number;
+  @Prop() readonly meetings!: string[];
+}
+</script>
+
+<style>
+#crs {
+  display: inline-block;
+  border: 2px solid hsl(30, 90%, 50%);
+  border-radius: 0.5em;
+  padding: 1em;
+  margin: 0.6em;
+  box-shadow: 3px 5px 6px 7px hsla(30, 55%, 50%, 0.4);
+}
+
+li {
+  list-style: none;
+  font-weight: bold;
+}
+</style>
